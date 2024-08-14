25506a8c642beba01c0fcebfe7d91e69f60bacc4
balram gupta <balramg5643@gmail.com>
2024-08-14 14:11:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loginpage/SignupActivity.kt
@@ -0,0 +1,54 @@
+package com.example.loginpage
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.loginpage.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignupActivity : AppCompatActivity() {
+    //
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.signupButton.setOnClickListener{
+            val email = binding.signupEmail.text.toString()
+            val password = binding.signupPassword.text.toString()
+            val confirmPassword = binding.signupConfirm.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+                if (password == confirmPassword){
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Password does not matched", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.loginRedirectText.setOnClickListener {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+    }
+}
